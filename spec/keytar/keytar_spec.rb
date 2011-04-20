369e1fe86519c211a388cb355a22571eae48f547
Schneems <richard.schneeman@gmail.com>
2011-04-20 10:54:09 -0500

    test ActiveRecord backed instances with keytar

--- spec/keytar/keytar_spec.rb
+++ spec/keytar/keytar_spec.rb
@@ -1,6 +1,17 @@
 require 'spec_helper'
+## Gives us ActiveRecord backed model Bar that we can test instances of
+ActiveRecord::Base.establish_connection(
+:adapter => "sqlite3",
+:database => ":memory:",
+:host     => 'localhost')
+ActiveRecord::Migrator.migrate('../../db/migrate', ENV["VERSION"] ? ENV["VERSION"].to_i : nil )
+## Gives us ActiveRecord backed model Bar that we can test instances of
+
+
 class Bar < ActiveRecord::Base
+
 end
+
 
 describe Keytar do
   describe 'requiring Keytar' do
@@ -8,6 +19,13 @@ describe Keytar do
       describe Bar.ancestors do
         it {should include( KeyBuilder)}
       end
+    end
+
+    it 'should ' do
+      name = "notblank"
+      b = Bar.create(:name => name)
+      b.name.should == name
+      b.awesome_key.should == "bars:awesome:#{b.id}"
     end
   end
 end
