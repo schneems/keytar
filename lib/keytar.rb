d14e2f9a5d4a2372fcab59bd69d73b3fdc65f127
Schneems <richard.schneeman@gmail.com>
2011-05-05 01:33:08 -0500

    allow non-rails ruby-projects to use keytar by making active record optional

--- lib/keytar.rb
+++ lib/keytar.rb
@@ -1,7 +1,10 @@
 require 'rubygems'
-require 'active_record'
+begin; require 'active_record' rescue; end
 
 module Keytar
   autoload :KeyBuilder, 'keytar/key_builder'
-  ActiveRecord::Base.class_eval %{ include KeyBuilder }
+  ActiveRecord::Base.class_eval { include KeyBuilder } if defined?(ActiveRecord::Base)
+  def self.included(klass)
+    klass.class_eval {include KeyBuilder}
+  end
 end
--- spec/keytar/keytar_spec.rb
+++ spec/keytar/keytar_spec.rb
@@ -14,20 +14,53 @@ class Bar < ActiveRecord::Base
 
 end
 
+class BarNonActiveRecord
+
+end
+
 
 describe Keytar do
   describe 'requiring Keytar' do
-    it 'should load keybuilder into ActiveRecord::Base' do
+    it 'should load keybuilder into ActiveRecord::Base if defined' do
       describe Bar.ancestors do
         it {should include( KeyBuilder)}
       end
     end
 
-    it 'should ' do
+    it 'should allow ActiveRecord based objects to use their unique identifiers' do
       name = "notblank"
       b = Bar.create(:name => name)
       b.name.should == name
       b.awesome_key.should == "bars:awesome:#{b.id}"
     end
   end
+
+  describe 'requiring Keytar with ActiveRecord undefined' do
+    before do
+      begin; Object.send(:remove_const, "ActiveRecord"); rescue NameError; end
+    end
+
+    it 'does not automatically add KeyBuilder to the class' do
+      describe BarNonActiveRecord.ancestors do
+        it {should_not include( KeyBuilder)}
+      end
+    end
+
+    describe 'allows non ActiveRecord based classes to use keytar directly' do
+      it 'includes keybuilder when it is included' do
+        BarNonActiveRecord.class_eval do
+          include Keytar
+        end
+        describe BarNonActiveRecord.ancestors do
+          it {should include( KeyBuilder)}
+        end
+      end
+
+      it 'should allow ActiveRecord based objects to use their unique identifiers' do
+        BarNonActiveRecord.awesome_key.should == "barnonactiverecord:awesome"
+      end
+    end
+  end
+
+
 end
