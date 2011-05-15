ae26a470a686fe08043998c417870262fc923907
Schneems <richard.schneeman@gmail.com>
2011-05-15 14:27:34 -0500

    Don't auto load keytar into activerecord, Must be specifically included in models

--- lib/keytar.rb
+++ lib/keytar.rb
@@ -1,8 +1,6 @@
-begin; require 'active_record'; rescue; end
 
 module Keytar
   autoload :KeyBuilder, 'keytar/key_builder'
-  ActiveRecord::Base.class_eval { include KeyBuilder } if defined?(ActiveRecord::Base)
   def self.included(klass)
     klass.class_eval {include KeyBuilder}
   end
