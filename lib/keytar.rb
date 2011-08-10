1f09ae7be7d022d03e79692231d2106b7856cb94
Schneems <richard.schneeman@gmail.com>
2011-08-09 23:42:48 -0500

    initial attempt at a rewrite

--- lib/keytar.rb
+++ lib/keytar.rb
@@ -3,8 +3,95 @@ autoload :KeyBuilder, 'keytar/key_builder'
 
 module Keytar
   extend ActiveSupport::Concern
+  DEFAULTS = {:delimiter  => ":",
+                :order    => [:shard, :prefix, :base, :name, :unique, :args, :suffix, :version, :v],
+                :pluralize_instances => true,
+                :key_case => :downcase,
+                :unique   => :id}
 
-  included do
-    include KeyBuilder
+  class Key
+    attr_accessor :delimiter, :order, :key_case, :options
+    def initialize(options = {})
+      options[:name]  = options.delete(:name).to_s.gsub(/(^key$|_key$)/, '')
+      self.delimiter  = options.delete(:delimiter)
+      self.order      = options.delete(:order)
+      self.key_case   = options.delete(:key_case)
+      self.options    = options
+    end
+
+    def key_array
+      order.map do |key|
+        if key != :args
+          options[key].try(:to_s)
+        else
+          options[key].map(&:to_s) unless options[key].blank?
+        end
+      end.flatten.compact
+    end
+
+    def to_s
+      key = key_array
+      key = key.send key_case if key_case.present?
+      key
+    end
+
+    def self.build(options = {})
+      self.new(options).to_s
+    end
+  end
+
+
+  def self.define_key_class_method_on(base, options = {})
+    (class << base;self ;end).instance_eval do
+      define_method("#{options[:name]}_key") do |*args|
+        build_key(options.merge(:args => args))
+      end
+    end
+  end
+
+  def self.define_key_instance_method_on(base, options)
+    base.class_eval do
+      define_method("#{options[:name]}_key") do |*args|
+        build_key(options.merge(:args => args))
+      end
+    end
+  end
+
+  # class methods to be extended
+  module ClassMethods
+    # sets up configuration options for individual keys
+    # alows us to define the keys without calling method missing
+    def define_keys(*args)
+      # coherce args into meaningful things
+      names = []; options = {}; args.each {|arg| arg.is_a?(Hash) ? options = arg : names << arg}
+      names.each do |name|
+        name = name.to_s.gsub(/(^key$|_key$)/, '')
+        KeyBuilder.define_key_class_method_on(self, options.merge(:name => name))
+        KeyBuilder.define_key_instance_method_on(self, options.merge(:name => name))
+      end
+    end
+    alias :define_key  :define_keys
+
+
+    # a way to define class level configurations for keytar using a hash
+    def key_config(options = {})
+        options[:base]  = self.class
+        @key_config     ||= options.reverse_merge(KeyBuilder::DEFAULTS)
+    end
+
+    # Call KeyBuilder.build_key or Foo.build_key with options
+    # :base => self.to_s.downcase, :name => method_name, :args => args
+    def build_key(options = {})
+      Key.build(options.reverse_merge(key_config))
+    end
+  end
+
+  # build_key method for instances by default class is pluralized to create different key
+  def build_key(options = {})
+    options.reverse_merge(self.class.key_config)
+    unique            = self.send (options.key_config[:key_unique].to_sym)
+    options[:base]    = options[:base].pluralize if options[:pluralize_instances]
+    options[:unique]  = unique unless unique == object_id
+    Key.build(options)
   end
 end
--- spec/keytar_spec.rb
+++ spec/keytar_spec.rb
@@ -43,15 +43,11 @@ describe Keytar do
     end
   end
 
-  describe 'cache_key' do
-    it 'allows us to pre-define class methods' do
-      Foo.cache_key(:cached_method, :delimiter => "/", :key_prefix => "woo")
-      Foo.respond_to?(:cached_method_key).should be_true
-      puts Foo.cached_method_key(22).should == "woo/foo/cached_method/22"
-    end
+  describe 'define_key' do
 
     it 'allows us to pre-define instance methods' do
-      Foo.cache_key(:cached_instance_method, :delimiter => "|", :version => "3")
+      pending
+      Foo.define_key(:cached_instance_method, :delimiter => "|", :version => "3")
       @foo = Foo.new
       @foo.respond_to?(:cached_instance_method_key).should be_true
       @foo.cached_instance_method_key.should == "foos|cached_instance_method|3"
@@ -59,7 +55,7 @@ describe Keytar do
 
     describe 'taking an array' do
       it 'allows us to pre-define multiple class key methods' do
-        Foo.cache_key(:m1, :m2, :m3, :delimiter => ":", :key_prefix => "foo")
+        Foo.define_key(:m1, :m2, :m3, :delimiter => ":", :key_prefix => "foo")
         Foo.respond_to?(:m1_key).should be_true
         Foo.respond_to?(:m2_key).should be_true
         Foo.respond_to?(:m3_key).should be_true
@@ -95,10 +91,12 @@ describe Keytar do
     end
 
     it 'should respond to "awesome_key" method by returning :class, :delimiter, :name' do
+      pending
       @foo.awesome_key.should == "foos:awesome"
     end
 
     it 'should respond to "awesome_key(number)" method by returning :class, :delimiter, :name, :delimiter, :arg' do
+      pending
       number = rand(100)
       @foo.awesome_key(number).should == "foos:awesome:#{number}"
     end
@@ -108,118 +106,12 @@ describe Keytar do
     end
   end
 
-  # test last
-  describe 'class configurations' do
-    after(:each) do
-      # todo find a better way of resetting all these values
-      Foo.key_delimiter KeyBuilder::DEFAULTS[:key_delimiter]
-      Foo.key_order KeyBuilder::DEFAULTS[:key_order]
-      Foo.key_prefix KeyBuilder::DEFAULTS[:key_prefix]
-      Foo.key_suffix KeyBuilder::DEFAULTS[:key_suffix]
-      Foo.key_pluralize_instances KeyBuilder::DEFAULTS[:key_pluralize_instances]
-      Foo.key_case KeyBuilder::DEFAULTS[:key_case]
-      Foo.key_plural KeyBuilder::DEFAULTS[:key_plural]
-      Foo.key_unique KeyBuilder::DEFAULTS[:key_unique]
-    end
-
-    it 'should change key_delimiter' do
-      key_delimiter = "|"
-      Foo.key_delimiter key_delimiter
-      Foo.key_delimiter.should == key_delimiter
-      Foo.awesome_key.should == "foo#{key_delimiter}awesome"
-    end
-
-
-    it 'should change key_order' do
-      order_array = [:prefix, :name, :unique, :base, :args, :suffix]
-      Foo.key_order order_array
-      Foo.key_order.should == order_array
-      Foo.awesome_key.should == "awesome:foo"
-    end
-
-    it 'should change key_prefix' do
-      key_prefix = "memcache"
-      Foo.key_prefix key_prefix
-      Foo.key_prefix.should == key_prefix
-      Foo.awesome_key.should == "#{key_prefix}:foo:awesome"
-    end
-
-    it 'should change key_suffix' do
-      key_suffix = "slave"
-      Foo.key_suffix key_suffix
-      Foo.key_suffix.should == key_suffix
-      Foo.awesome_key.should == "foo:awesome:#{key_suffix}"
-    end
-
-     it 'should change key_pluralize_instances' do
-       key_pluralize_instances = false
-       Foo.key_pluralize_instances key_pluralize_instances
-       Foo.key_pluralize_instances.should == key_pluralize_instances
-       foo = Foo.new
-       foo.awesome_key.should == "foo:awesome"
-     end
-
-     it 'should change key_case' do
-       key_case = :upcase
-       Foo.key_case key_case
-       Foo.key_case.should == key_case
-       Foo.awesome_key.should == "FOO:AWESOME"
-     end
-
-     it 'should change key_plural' do
-       key_plural = "fooz"
-       Foo.key_plural key_plural
-       Foo.key_plural.should == key_plural
-       foo = Foo.new
-       foo.awesome_key.should == "fooz:awesome"
-     end
-
-     it 'should change key_unique' do
-       Foo.class_eval { def timeish; (Time.now.to_i * 0.01).floor.to_s; end}
-       key_unique = :timeish
-       Foo.key_unique key_unique
-       Foo.key_unique.should == key_unique
-       foo = Foo.new
-       foo.awesome_key.should include(foo.timeish)
-     end
-
-     # todo move tests and assertsions to seperate describe and it blocks
-     it 'should allow all configurations to be set using a hash' do
-       # variables
-       key_delimiter = "/"
-       key_order = [:prefix, :base, :suffix]
-       key_prefix = "before"
-       key_suffix = "after"
-       key_pluralize_instances = false
-       key_case = :upcase
-       key_plural = "zoosk"
-       key_unique = "doesn-t_apply_to_instance_methods"
-       # config
-       Foo.keyfig :delimiter => key_delimiter,
-                  :order => key_order,
-                  :prefix => key_prefix,
-                  :suffix => key_suffix,
-                  :pluralize_instances => key_pluralize_instances,
-                  :case => key_case,
-                  :plural => key_plural,
-                  :unique => key_unique
-      # assertions
-      Foo.key_delimiter.should == key_delimiter
-      Foo.key_order.should == key_order
-      Foo.key_prefix.should == key_prefix
-      Foo.key_suffix.should == key_suffix
-      Foo.key_pluralize_instances.should == key_pluralize_instances
-      Foo.key_case.should == key_case
-      Foo.key_plural.should == key_plural
-      Foo.key_unique.should == key_unique
-     end
-
-  end
 
 
 
   describe 'requiring Keytar' do
     it 'should allow ActiveRecord based objects to use their unique identifiers' do
+      pending
       name = "notblank"
       b = Bar.create(:name => name)
       b.name.should == name
@@ -267,4 +159,13 @@ describe Keytar do
     end
   end
 
+
+  describe ' advanced args' do
+   it '' do
+     args = {:this_is_a => :hash}
+     puts "================================="
+  #   puts Foo.build_key(:name => "test", :args => args, :base => "foo").should eq("foo:test:{:this_is_a => hash}")
+   end
+ 
+  end
 end
